c773720bd282b22e722167f520fc9ead786d1e4c
riya40 <priyanunna20@gmail.com>
2021-12-07 19:14:51 +0530

    Added:Welcome message

--- src/com/bridgelabz/demo/LineComparision.java
+++ src/com/bridgelabz/demo/LineComparision.java
@@ -3,6 +3,7 @@ package com.bridgelabz.demo;
 public class LineComparision {
 
     public static void main(String[] args) {
+
 	// Displaying the Welcome Message
         System.out.println("welcome to Line Comparision Computation");
     }
