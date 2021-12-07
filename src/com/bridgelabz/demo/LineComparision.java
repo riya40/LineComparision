dd09e50e3fa8ff8fa012ec4746657aed77a82c63
riya40 <priyanunna20@gmail.com>
2021-12-07 21:06:21 +0530

    Added:Calculating The Length Of The Two Lines

--- src/com/bridgelabz/demo/LineComparision.java
+++ src/com/bridgelabz/demo/LineComparision.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.demo;
+import java.util.Scanner;
+import java.lang.Math;
+
 
 public class LineComparision {
 
@@ -6,5 +9,22 @@ public class LineComparision {
 
 	// Displaying the Welcome Message
         System.out.println("welcome to Line Comparision Computation");
+
+        int x1,x2,y1,y2;
+        double r;
+
+        System.out.println("enter first value");
+        Scanner sc=new Scanner(System.in);
+        x1=sc.nextInt();
+        System.out.println("enter second value");
+        x2=sc.nextInt();
+        System.out.println("enter third point");
+        y1=sc.nextInt();
+        System.out.println("enter fourth point");
+        y2=sc.nextInt();
+        int a=x2-x1;
+        int b=y2-y1;
+        r=(Math.sqrt(Math.pow(a, 2)+Math.pow(b, 2)));
+        System.out.println("the length:" +r);
     }
 }
